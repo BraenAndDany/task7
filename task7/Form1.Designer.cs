d57b192df2c4175b1213c08060b045a123cbc019
Max <Xetyq@yandex.ru>
2023-12-06 11:38:39 +0700

    Добавьте файлы проекта.

---
+++ task7/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace task7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(516, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 40);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(290, 125);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(290, 22);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "C:\\Users\\User\\Downloads\\icosa-tab-20220408-rel";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(334, 12);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(294, 153);
+            this.richTextBox3.TabIndex = 3;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 231);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+    }
+}
+
---
+++ task7/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using LibreHardwareMonitor.Hardware;
+
+namespace task7
+{
+    public partial class Form1 : Form
+    {
+        class ProgramTask7
+        {
+            public string FindLargestFile(string directory)
+            {
+                // Проверяет, существует ли директория
+                if (Directory.Exists(directory))
+                {
+                    // Получает все файлы в директории и их размеры
+                    var files = Directory.GetFiles(directory);
+                    var sizes = files.Select(f => new FileInfo(f).Length);
+
+                    // Находит максимальный размер и соответствующий файл
+                    var max = sizes.Max();
+                    var index = sizes.ToList().IndexOf(max);
+                    var file = files[index];
+
+                    // Возвращает имя и размер файла в виде строки
+                    return $"Самый большой файл в директории {directory} это {file}, его размер {max} байт";
+                }
+                else
+                {
+                    // Возвращает сообщение об ошибке, если директория не существует
+                    return $"Директория {directory} не существует";
+                }
+            }
+
+            // Переименовывает все файлы внутри какой-либо директории от 1 до N, где N – это количество файлов в директории
+            public string RenameFiles(string directory)
+            {
+            // Проверяет, существует ли директория
+                if (Directory.Exists(directory))
+                {
+                    // Получает все файлы в директории и их расширения
+                    var files = Directory.GetFiles(directory);
+                    var extensions = files.Select(f => Path.GetExtension(f));
+
+                    // Переименовывает каждый файл с помощью цикла for
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        // Формирует новое имя файла с помощью индекса и расширения
+                        var newName = (i + 1).ToString() + extensions.ElementAt(i);
+
+                    // Переименовывает файл с помощью метода Move
+                    try
+                    {
+                        File.Move(files[i], Path.Combine(directory, newName));
+                    }
+                    catch { 
+                    }
+                    }
+
+                    // Возвращает сообщение об успехе
+                    return $"Все файлы в директории {directory} были переименованы от 1 до {files.Length}";
+                }
+                else
+                {
+                    // Возвращает сообщение об ошибке, если директория не существует
+                    return $"Директория {directory} не существует";
+                }          
+            }
+
+            static CpuInfo GetCpuInfo()
+            {
+                Computer computer = new Computer
+                {
+                    IsCpuEnabled = true // Включаем мониторинг CPU
+                };
+                computer.Open(); // Начинаем мониторинг
+
+                CpuInfo cpuInfo = new CpuInfo();
+
+                foreach (var hardwareItem in computer.Hardware)
+                {
+                    if (hardwareItem.HardwareType == HardwareType.Cpu)
+                    {
+                        hardwareItem.Update(); // Обновляем информацию о CPU
+                        foreach (var sensor in hardwareItem.Sensors)
+                        {
+                            if (sensor.SensorType == SensorType.Temperature)
+                            {
+                                cpuInfo.Temperature = sensor.Value.HasValue ? sensor.Value.Value : 0;
+                            }
+                            else if (sensor.SensorType == SensorType.Load)
+                            {
+                                cpuInfo.Load = sensor.Value.HasValue ? sensor.Value.Value : 0;
+                            }
+                        }
+                        cpuInfo.Name = hardwareItem.Name;
+                    }
+                }
+
+                computer.Close(); // Заканчиваем мониторинг
+                return cpuInfo;
+            }       
+
+            public class CpuInfo
+            {
+                public double Temperature { get; set; }
+                public double Load { get; set; }
+                public string Name { get; set; }
+            }
+
+            public string ain()
+            {
+                string StringCpuInfo = "";
+                var cpuInfo = GetCpuInfo();
+                Console.WriteLine("Температура процессора: {0} °C", cpuInfo.Temperature);
+                Console.WriteLine("Загрузка процессора: {0} %", cpuInfo.Load);
+                Console.WriteLine("Название процессора: {0}", cpuInfo.Name);
+                StringCpuInfo = "Название процессора: " + cpuInfo.Name + "               Загрузка процессора: " + cpuInfo.Load + "%              Температура процессора: " + cpuInfo.Temperature;
+                return StringCpuInfo;
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProgramTask7 GetData = new ProgramTask7();
+            richTextBox1.Text += Environment.NewLine;
+            richTextBox1.Text += Environment.NewLine;
+            richTextBox1.Text += GetData.RenameFiles(richTextBox2.Text);
+            richTextBox1.Text += Environment.NewLine;
+            richTextBox1.Text += Environment.NewLine;
+            richTextBox1.Text = GetData.FindLargestFile(richTextBox2.Text);
+            richTextBox3.Text = GetData.ain();
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
